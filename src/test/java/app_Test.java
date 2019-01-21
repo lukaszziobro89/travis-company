a39dbdbc0581b1900f9b1f25c6090b1139affe15
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-21 21:42:09 +0100

    junit sample test

---
+++ src/test/java/app_Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class app_Test {
+    @Test
+    public void TEST_simpleTest(){
+        assertEquals(1,1);
+    }
+}

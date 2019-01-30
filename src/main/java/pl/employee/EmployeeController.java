d1180fbb71505ca8451ed007da6deeecda4f6135
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-30 21:26:13 +0100

    refactoring for webapp

--- src/main/java/pl/employee/EmployeeController.java
+++ src/main/java/pl/employee/EmployeeController.java
@@ -1,4 +1,39 @@
 package pl.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.util.List;
+
+@Controller
 public class EmployeeController {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    /**
+     *  Init binder makes sure that not white spaces are submitted in form.
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping(value = "/showEmployees")
+    public String getEmployees(Model theModel) {
+
+        List<Employee> theEmployees = employeeDAO.getEmployees();
+        theModel.addAttribute("employee", theEmployees);
+        return "employeesList";
+    }
 }

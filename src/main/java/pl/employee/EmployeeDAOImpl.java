b081d395b1c2f098e2b54cfa3d649cb9f2d0b272
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-30 21:26:42 +0100

    refactoring for webapp

--- src/main/java/pl/employee/EmployeeDAOImpl.java
+++ src/main/java/pl/employee/EmployeeDAOImpl.java
@@ -1,4 +1,38 @@
 package pl.employee;
 
-public class EmployeeDAOImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public EmployeeDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public List<Employee> getEmployees() {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // create a query
+        Query<Employee> theQuery =
+                currentSession.createQuery("from Employee", Employee.class);
+
+        // execute query and get result list
+        List<Employee> employees = theQuery.getResultList();
+
+        // return the results
+        return employees;
+    }
 }
